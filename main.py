1247f4b1056f344bd5650bb17560cb327265bd14
Underdog-000 <ilonmussk20@gmail.com>
2026-04-26 00:01:58 +0300

    Load ESPN extension routes from main entrypoint

--- main.py
+++ main.py
@@ -4,3 +4,4 @@ import soccerdata_extensions  # noqa: F401 - registers soccerdata routes on app
 import soccerdata_alias_patch  # noqa: F401 - patches soccerdata team aliases
 import understat_extensions  # noqa: F401 - registers soccerdata Understat routes on app
 import fbref_extensions  # noqa: F401 - registers soccerdata FBref routes on app
+import espn_extensions  # noqa: F401 - registers soccerdata ESPN routes on app
